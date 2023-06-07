65d5721a96118459714b8d15b71bb5e57a910ff3
sako-ranj <sakoranj2001@gmail.com>
2023-06-07 19:53:35 +0300

    path & query parameters, added requirements file

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from fastapi import FastAPI
-
+from typing import Optional
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return {'data': {'name': 'sako'}}
+@app.get("/blog")
+def index(limit, published: bool=True, sort: Optional[str]=None):
+    if published:
+        return {"data": f"{limit} published blogs from the db"}
+    else:
+        return {"data": f"{limit} blogs from the db"}
 
-@app.get('/about')
-def about():
-    return {'data':'about page'}
+
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data", "asdf"}
+
+
+@app.get("/blog/{id}")
+def show(id: int):
+    return {"data": id}
+
+
+@app.get("/blog/{id}/comments")
+def comments(id,limit=10):
+    return {"data": {"1", "2"}}
